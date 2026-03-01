6b089288026634a5f42e418f6f7bb71387a6aac6
orowader-a11y <orowader@gmail.com>
2026-03-01 01:41:18 -0500

    Update build_exe.bat

--- build_exe.bat
+++ build_exe.bat
@@ -7,6 +7,7 @@ if not exist venv (
 call venv\Scripts\activate
 python -m pip install --upgrade pip
 pip install -r requirements.txt
+REM requirements.txt includes python-sonarqube-api for optional SonarQube server integration (config: sonar.url, sonar.projectKey, SONAR_TOKEN)
 pyinstaller --noconfirm --windowed --onefile --name FrontendZipSecurityAnalyzer app.py
 
 echo Build finished. EXE is in dist\FrontendZipSecurityAnalyzer.exe
